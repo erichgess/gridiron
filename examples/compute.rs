9e3899a5d972d00f5409e8c3b6c191201c76e5db
Jonathan Zrake <jonathan.zrake@gmail.com>
2021-04-02 17:54:26 -0400

    Work on the compute trait and example

--- examples/compute.rs
+++ examples/compute.rs
@@ -1,11 +1,21 @@
+#![feature(drain_filter)]
+
+
+
+
 use core::hash::Hash;
 use std::collections::HashMap;
+use std::sync::mpsc;
 use rayon::prelude::*;
 
 
 
 
-trait ComputeStage {
+/**
+ * Interface for a task which can be performed in parallel with a group of
+ * peers. Each task may require a subset of its peers to be evaluated.
+ */
+trait Compute {
 
     type Key;
 
@@ -13,7 +23,7 @@ trait ComputeStage {
 
     fn key(&self) -> Self::Key;
 
-    fn peer_keys(&self) -> &[Self::Key];
+    fn peer_keys(&self) -> Vec<Self::Key>;
 
     fn run(&self, peers: Vec<&Self>) -> Self::Value;
 }
@@ -21,7 +31,8 @@ trait ComputeStage {
 
 
 
-fn get_all<'a, K, V>(map: &'a HashMap<K, V>, keys: &[K]) -> Option<Vec<&'a V>>
+// ============================================================================
+fn get_all<'a, K, V>(map: &'a HashMap<K, V>, keys: Vec<K>) -> Option<Vec<&'a V>>
 where
     K: Hash + Eq,
 {
@@ -33,10 +44,74 @@ where
 
 
 
+// ============================================================================
+fn execute_one_stage_par_channel_internal<C, K, V>(stage: mpsc::Receiver<(K, C)>) -> mpsc::Receiver<(K, V)>
+where
+    C: 'static + Sync + Send + Clone + Compute<Key = K, Value = V>,
+    K: 'static + Sync + Send + Clone + Hash + Eq + std::fmt::Debug,
+    V: 'static + Send
+{
+    let (send, source) = mpsc::channel();
+    let (sink, output) = mpsc::channel();
+
+
+    std::thread::spawn(move || {
+        source
+        .into_iter()
+        .par_bridge()
+        .for_each_with(sink, |sink, (key, item, peers): (K, C, Vec<C>)| {
+            sink.send((key.clone(), item.run(peers.iter().collect()))).unwrap();
+        });
+    });
+
+
+    let mut seen: HashMap<K, C> = HashMap::new();
+    let mut hold = Vec::new();
+
+
+    for (key, item) in stage {
+        seen.insert(key.clone(), item);
+        hold.push(key);
+        hold.drain_filter(|key| {
+            let held_item = seen.get(key).unwrap();
+            if let Some(peers) = get_all(&seen, held_item.peer_keys()) {
+                send.send((key.clone(), held_item.clone(), peers.into_iter().cloned().collect())).unwrap();
+                true
+            } else {
+                false
+            }
+        });
+    }
+
+
+    assert!(hold.is_empty(), "there were {} unevaluated computes", hold.len());
+    output
+}
+
+
+
+
+// ============================================================================
+fn execute_one_stage_channel<C, K, V>(stage: HashMap<K, C>) -> HashMap<K, V>
+where
+    C: 'static + Sync + Send + Clone + Compute<Key = K, Value = V>,
+    K: 'static + Sync + Send + Clone + Hash + Eq + std::fmt::Debug,
+    V: 'static + Send
+{
+    let (s, r) = mpsc::channel();
+
+    for kv in stage {
+        s.send(kv).unwrap();
+    }
+    drop(s);
+    execute_one_stage_par_channel_internal(r).into_iter().collect()
+}
+
+// ============================================================================
 fn execute_one_stage_par<C, K, V>(stage: HashMap<K, C>) -> HashMap<K, V>
 where
-    C: Sync + ComputeStage<Key = K, Value = V>,
-    K: Send + Sync + Hash + Eq + Clone,
+    C: Compute<Key = K, Value = V> + Sync,
+    K: Sync + Send + Hash + Eq + Clone,
     V: Send
 {
     stage.par_iter().map(|(k, compute)| {
@@ -44,27 +119,10 @@ where
     }).collect()
 }
 
-
-
-
-fn execute_two_stage_par<C, D, K, V>(stage_a: HashMap<K, C>) -> HashMap<K, V>
+// ============================================================================
+fn execute_one_stage_ser<C, K, V>(stage: HashMap<K, C>) -> HashMap<K, V>
 where
-    C: Sync + ComputeStage<Key = K, Value = D>,
-    D: Sync + ComputeStage<Key = K, Value = V> + Send,
-    K: Send + Sync + Hash + Eq + Clone,
-    V: Send
-{
-    let stage_b = execute_one_stage_par(stage_a);
-    let stage_c = execute_one_stage_par(stage_b);
-    stage_c
-}
-
-
-
-
-fn execute_one_stage<C, K, V>(stage: HashMap<K, C>) -> HashMap<K, V>
-where
-    C: ComputeStage<Key = K, Value = V>,
+    C: Compute<Key = K, Value = V>,
     K: Hash + Eq + Clone,
 {
     stage.iter().map(|(k, compute)| {
@@ -75,187 +133,32 @@ where
 
 
 
-fn execute_two_stage<C, D, K, V>(stage_a: HashMap<K, C>) -> HashMap<K, V>
-where
-    C: ComputeStage<Key = K, Value = D>,
-    D: ComputeStage<Key = K, Value = V>,
-    K: Hash + Eq + Clone,
-{
-    let stage_b = execute_one_stage(stage_a);
-    let stage_c = execute_one_stage(stage_b);
-    stage_c
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/**
- * A custom coroutine proof-of-concept
- */
-trait Compute: Sized {
-
-    /// Associated type to uniquely identify this compute task to its peers.
-    type Key;
-
-    /// Type of the result eventually yielded from this compute task.
-    type Value;
-
-    /// Return this compute task's key.
-    fn key(&self) -> Self::Key;
-
-    /// Return the upstream keys required for this task to proceed to the next
-    /// stage.
-    fn upstream(&self) -> Vec<Self::Key>;
-
-    /// Upload the peer values associated with the upstream keys. Note that
-    /// the upstream keys can change after a call to `advance`.
-    fn upload(&mut self, upstream: Vec<Self>);
-
-    /// Return this compute tasks's state, containing the result if it's ready.
-    fn state(&mut self) -> Option<Self::Value>;
-
-    /// Advance this compute task's internal state by performing an expensive
-    /// calculation. The result of `upstream` can change each time this method
-    /// is invoked.
-    fn advance(&mut self);
-
-    /// Convenience provided method to consume this compute, advance and return
-    /// it.
-    fn into_advance(mut self) -> Self {
-        self.advance();
-        self
-    }
-}
-
-
-
-
+// ============================================================================
 #[derive(Clone)]
-
-
-/**
- * An example compute object. It's a crude state machine.
- */
-struct MyCompute {
-    key: i64,
-    value: f64,
-    upstream: Option<Vec<f64>>,
-    result: Option<f64>,
+struct StringConvolve {
+    index: usize,
+    group_size: usize,
 }
 
 
 
 
 // ============================================================================
-impl MyCompute {
-    fn new(key: i64, value: f64) -> Self {
-        Self {
-            key,
-            value,
-            upstream: None,
-            result: None,
-        }
-    }
-}
+impl Compute for StringConvolve {
 
-
-
-
-// ============================================================================
-impl Compute for MyCompute {
-
-    type Key = i64;
-    type Value = f64;
+    type Key = usize;
+    type Value = String;
 
     fn key(&self) -> Self::Key {
-        self.key
+        self.index
     }
 
-    fn upstream(&self) -> Vec<Self::Key> {
-        vec![(self.key + 100 - 1) % 100, (self.key + 1) % 100]
+    fn peer_keys(&self) -> Vec<Self::Key> {
+        vec![(self.index + self.group_size - 1) % self.group_size, (self.index + 1) % self.group_size]
     }
 
-    fn upload(&mut self, upstream: Vec<Self>) {
-        self.upstream = Some(upstream.into_iter().map(|peer| peer.value).collect())
-    }
-
-    fn advance(&mut self) {
-        if let Some(upstream) = self.upstream.take() {
-            self.result = Some((upstream[0] + self.value + upstream[1]) / 3.0)
-        }
-    }
-
-    fn state(&mut self) -> Option<Self::Value> {
-        self.result
-    }
-}
-
-
-
-
-/**
- * Drive a collection of interdependent state machines.
- */
-fn execute<C, K>(computes: Vec<C>) -> Vec<(C::Key, C::Value)>
-where
-    C: Compute<Key = K> + Send + Clone,
-    K: Hash + Eq + Send,
-{
-    let num_tasks = computes.len();
-    let mut results = Vec::new();
-
-    let mut stage_a : HashMap<C::Key, C>;
-    let mut stage_b = HashMap::new();
-
-    stage_a = computes
-        .into_iter()
-        .map(|c| (c.key(), c.into_advance()))
-        .collect();
-
-    loop {
-        let mut progress = false;
-
-        for (_, compute) in &stage_a {
-
-            let upstream: Option<Vec<_>> = compute
-                .upstream()
-                .iter()
-                .map(|key| stage_a.get(key).cloned())
-                .collect();
-
-            let mut next = compute.clone();
-
-            next.upload(upstream.expect("missing upstream keys!"));
-            next.advance();
-
-            if let Some(result) = next.state() {
-                progress = true;
-                results.push((next.key(), result))
-            }
-            stage_b.insert(next.key(), next);
-        }
-
-        if stage_b.len() == num_tasks {
-            return results
-        }
-        if !progress {
-            panic!("computation stage did not make progress")
-        }
-        std::mem::swap(&mut stage_a, &mut stage_b);
+    fn run(&self, peers: Vec<&Self>) -> Self::Value {
+        format!("{} {} {}", peers[0].index, self.index, peers[1].index)
     }
 }
 
@@ -265,9 +168,95 @@ where
 // ============================================================================
 fn main() {
 
-    let computes: Vec<_> = (0..100).map(|i| MyCompute::new(i, i as f64)).collect();
+    let group_size = 10;
+    let stage_a = (0..group_size).map(|index| StringConvolve { index, group_size });
 
-    for (key, x) in execute(computes) {
-        println!("{} -> {}", key, x);
+    println!("\n--------------------------------------------");
+    {
+        let stage_a: HashMap<_, _> = stage_a.clone().map(|c| (c.key(), c)).collect();
+        let stage_b = execute_one_stage_channel(stage_a);
+
+        for (key, result) in stage_b {
+            println!("{} -> {}", key, result);        
+        }
+    }
+    println!("\n--------------------------------------------");
+
+    {
+        let stage_a: HashMap<_, _> = stage_a.clone().map(|c| (c.key(), c)).collect();
+        let stage_b = execute_one_stage_ser(stage_a);
+
+        for (key, result) in stage_b {
+            println!("{} -> {}", key, result);        
+        }
+    }
+    println!("\n--------------------------------------------");
+
+    {
+        let stage_a: HashMap<_, _> = stage_a.clone().map(|c| (c.key(), c)).collect();
+        let stage_b = execute_one_stage_par(stage_a);
+
+        for (key, result) in stage_b {
+            println!("{} -> {}", key, result);        
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// fn execute_two_stage<C, D, K, V>(stage_a: HashMap<K, C>) -> HashMap<K, V>
+// where
+//     C: Compute<Key = K, Value = D>,
+//     D: Compute<Key = K, Value = V>,
+//     K: Hash + Eq + Clone,
+// {
+//     let stage_b = execute_one_stage(stage_a);
+//     let stage_c = execute_one_stage(stage_b);
+//     stage_c
+// }
+
+
+
+
+// fn execute_two_stage_par<C, D, K, V>(stage_a: HashMap<K, C>) -> HashMap<K, V>
+// where
+//     C: Compute<Key = K, Value = D> + Sync,
+//     D: Compute<Key = K, Value = V> + Sync + Send,
+//     K: Sync + Send + Hash + Eq + Clone,
+//     V: Send
+// {
+//     let stage_b = execute_one_stage_par(stage_a);
+//     let stage_c = execute_one_stage_par(stage_b);
+//     stage_c
+// }
