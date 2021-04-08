491bfad0625f2f20cd993b7f26c721cd1f8bc30f
Jonathan Zrake <jonathan.zrake@gmail.com>
2021-04-08 16:53:04 -0400

    Add doc strings to the automata module

--- src/automaton.rs
+++ src/automaton.rs
@@ -18,18 +18,49 @@ pub enum Receipt<K> {
  */
 pub trait Automaton {
 
+
+    /// The type of the key to uniquely identify this automaton within a
+    /// group. Executors will generally require this type to be `Hash + Eq`,
+    /// and also `Send` if the executor is multi-threaded.
     type Key;
 
+
+    /// The type of a message to be passed between the automata. Each stage of
+    /// computation requires the receipt of zero or one messages from the
+    /// other automata in the group in order to yield a value.
     type Message;
 
+
+    /// The type of the value yielded by this automaton. Generation of the
+    /// yielded value is expected to be CPU-intensive, and may be carried on a
+    /// worker thread at the discretion of the executor. For the computation
+    /// to proceed requires the initial data on this task, and the messages it
+    /// recieved from its peers, 
     type Value;
 
+
+    /// Return the key uniquely identify this automaton within the group.
     fn key(&self) -> Self::Key;
 
+
+    /// Return a list of messages to be sent to peers.
     fn messages(&self) -> Vec<(Self::Key, Self::Message)>;
 
+
+    /// This method must be implemented to receive a message. This task should
+    /// take ownership of the message and keep it until a call to
+    /// `Self::value` is made by the executor. This method returns a receipt
+    /// enum indicating either Eligible or Ineligible, depending on whether
+    /// all of the anticipated messages have now been received. The key is
+    /// returned so that the executor does not need to assume `Key: Clone`.
+    /// This method will be invoked once by the executor for each incoming
+    /// message.
     fn receive(&mut self, message: (Self::Key, Self::Message)) -> Receipt<Self::Key>;
 
+
+    /// Run this task, given an owned vector of its peers. The executor is
+    /// responsible for making sure the order of the peers is the same as the
+    /// order of the `Vec` returned by the `peer_keys` method.
     fn value(self) -> Self::Value;
 }
 
@@ -37,7 +68,7 @@ pub trait Automaton {
 
 
 /**
- * Execute a group of automata
+ * Execute a group of automata in serial.
  */
 pub fn execute<I, A, K, V>(stage: I) -> impl Iterator<Item = V>
 where
@@ -60,7 +91,7 @@ where
 
 
 /**
- * Execute a group of automata in parallel
+ * Execute a group of automata in parallel.
  */
 pub fn execute_par<'a, I, A, K, V>(scope: &rayon::Scope<'a>, stage: I) -> impl Iterator<Item = V>
 where
@@ -79,6 +110,10 @@ where
         .into_iter()
         .par_bridge()
         .for_each(|peer: A| {
+            // NOTE: would there be a performance change if this were done
+            // using for_each_with to clone the sink? As it is, the sink is
+            // owned by the spawned closure, and captured by reference in the
+            // for_each.
             computed_sink.send(peer.value()).unwrap();
         });
     });
