0155543d65dfafdcb368d5e627545d41d37fe171
Erich Gerhard Ess <erich.g.ess@gmail.com>
2021-05-02 12:45:33 -0400

    In Progress: moving the TCP layer out of TcpCommunicator so that it can run in a thread safe manner while TcpCommunicator communicates with it through channels

--- src/message/tcp.rs
+++ src/message/tcp.rs
@@ -8,17 +8,57 @@ use std::thread;
 type Sender = mpsc::Sender<(usize, Vec<u8>)>;
 type Receiver = mpsc::Receiver<(usize, Vec<u8>)>;
 
+pub struct TcpHost {
+    send_sink: mpsc::Sender<(usize, Vec<u8>)>,
+    recv_src: mpsc::Receiver<Vec<u8>>,
+    listen_thread: thread::JoinHandle<()>,
+    send_thread: thread::JoinHandle<()>,
+}
+
+impl TcpHost {
+    pub fn new(rank: usize, peers: Vec<SocketAddr>) -> Self {
+        let (send_sink, send_src): (Sender, Receiver) = mpsc::channel();
+        let peers_cpy = peers.clone();
+        let send_thread = thread::spawn(move || {
+            for (rank, message) in send_src {
+                let mut stream = TcpStream::connect(peers_cpy[rank]).unwrap();
+                stream.write_all(&message.len().to_le_bytes()).unwrap();
+                stream.write_all(&message).unwrap();
+            }
+        });
+
+        let (recv_sink, recv_src) = mpsc::channel();
+        let listen_thread = thread::spawn(move || {
+            let listener = TcpListener::bind(peers[rank]).unwrap();
+            loop {
+                let (mut stream, _) = listener.accept().unwrap(); // There is a race condition here
+                let size = util::read_usize(&mut stream);
+                let bytes = util::read_bytes_vec(&mut stream, size);
+                match recv_sink.send(bytes) {
+                    Ok(_) => (),
+                    Err(_) => break,
+                }
+            }
+        });
+
+        TcpHost {
+            send_sink,
+            recv_src,
+            send_thread,
+            listen_thread,
+        }
+    }
+}
+
 pub struct TcpCommunicator {
     rank: usize,
     num_peers: usize,
-    listener: TcpListener,
     send_sink: Option<mpsc::Sender<(usize, Vec<u8>)>>,
     send_thread: Option<thread::JoinHandle<()>>,
 }
 
 impl TcpCommunicator {
     pub fn new(rank: usize, peers: Vec<SocketAddr>) -> Self {
-        let listener = TcpListener::bind(peers[rank]).unwrap();
         let num_peers = peers.len();
         let (send_sink, recv_sink): (Sender, Receiver) = mpsc::channel();
         let send_thread = thread::spawn(move || {
@@ -31,7 +71,6 @@ impl TcpCommunicator {
         Self {
             rank,
             num_peers,
-            listener,
             send_sink: Some(send_sink),
             send_thread: Some(send_thread),
         }
@@ -56,9 +95,7 @@ impl Communicator for TcpCommunicator {
     }
 
     fn recv(&self) -> Vec<u8> {
-        let (mut stream, _) = self.listener.accept().unwrap();
-        let size = util::read_usize(&mut stream);
-        util::read_bytes_vec(&mut stream, size)
+        vec![]
     }
 }
 
