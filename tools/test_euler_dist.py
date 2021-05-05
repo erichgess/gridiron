3d48c3a5177a49e3f2fc058984fc1f3022968f00
Erich Gerhard Ess <erich.g.ess@gmail.com>
2021-05-05 13:57:38 -0400

    Update the test script, this will now run an Euler cluster then, when the run is complete, stitch the output files together and open the plot view.  This will make it easier to validate that the output is correct on test runs'

--- tools/test_euler_dist.py
+++ tools/test_euler_dist.py
@@ -3,6 +3,8 @@
 import argparse
 import subprocess
 import time
+import glob
+import os
 
 
 def parse_args():
@@ -11,6 +13,27 @@ def parse_args():
     parser.add_argument('-n', '--number', default="1", required=True)
     args = parser.parse_args()
     return int(args.number)
+
+
+def check_for_old_results():
+    cbor_files = glob.glob("./*.cbor")
+    return len(cbor_files) > 0
+
+
+def get_result_files():
+    results = glob.glob("./state-rank-*.cbor")
+    return results
+
+
+def stitch_results(results):
+    cmd = ["./tools/stitch_euler.py", "-f {}".format(str.join(" ", results))]
+    run_cmd(cmd).wait()
+    os.rename("./stitched.cbor", "./state.cbor")
+
+
+def show_chart():
+    cmd = ["./tools/plot_euler.py"]
+    run_cmd(cmd).wait()
 
 
 def euler_cmd(t, n, b, peers, rank):
@@ -31,6 +54,11 @@ def run_cmd(cmd):
 
 number = parse_args()
 
+# If old results are in the directory don't run
+if check_for_old_results():
+    print("There are still old CBOR files from previous runs.  Please delete this files so that there is no contamination or accidental loss of old test results")
+    exit()
+
 peers = ["127.0.0.1:{}".format(8000 + i) for i in range(0, number)]
 print(peers)
 
@@ -45,3 +73,7 @@ for c in cmds:
     out.append(run_cmd(c))
 
 [o.wait() for o in out]
+
+results = get_result_files()
+stitch_results(results)
+show_chart()
