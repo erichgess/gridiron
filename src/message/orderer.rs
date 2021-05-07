0db9076907dde3fd3dc533e8419095b2b74947f8
Erich Gerhard Ess <erich.g.ess@gmail.com>
2021-05-07 17:25:34 -0400

    Changed the channel buffer that was used to store out of order messages with a HashMap buffer, where messages that are for a future iteration are stored and will be forwarded to workers when their iteration arrives.  This is superior to the previous version, because that created a busy loop whenever there were future messages.  This new version will take a one time action to store messages and will only reprocess them when they are needed

--- src/message/orderer.rs
+++ src/message/orderer.rs
@@ -1,10 +1,15 @@
-use std::sync::{
-    atomic::{AtomicUsize, Ordering},
-    Arc,
+use std::{
+    collections::HashMap,
+    sync::{
+        atomic::{AtomicUsize, Ordering},
+        Arc, Mutex,
+    },
 };
 
 use crossbeam_channel::{Receiver, Sender};
 use log::error;
+
+use super::tcp::Iteration;
 
 #[derive(serde::Serialize, serde::Deserialize)]
 pub struct Envelope {
@@ -17,32 +22,24 @@ pub struct Envelope {
 /// workers have reached that iteration.
 pub struct Orderer {
     cur_iteration: Arc<AtomicUsize>,
+    buffer: Arc<Mutex<HashMap<Iteration, Vec<Vec<u8>>>>>,
+    sink: Sender<Vec<u8>>,
 }
 
 impl Orderer {
     pub fn new(initial_iteration: usize, chan: Receiver<Envelope>) -> (Orderer, Receiver<Vec<u8>>) {
         let cur_iteration = Arc::new(AtomicUsize::new(initial_iteration));
-        let (buffer_in, buffer_out): (Sender<Envelope>, Receiver<Envelope>) =
-            crossbeam_channel::unbounded();
 
-        {
-            let bc = buffer_in.clone();
-            std::thread::spawn(move || {
-                for env in chan {
-                    bc.send(env).unwrap();
-                }
-            });
-        }
-
+        let buffer = Arc::new(Mutex::new(HashMap::new()));
         let (arrival_in, arrival_out) = crossbeam_channel::unbounded();
         {
-            let (bic, boc) = (buffer_in.clone(), buffer_out.clone());
             let aic = arrival_in.clone();
             let c_iter = Arc::clone(&cur_iteration);
+            let buffer = Arc::clone(&buffer);
 
             // TODO: note that this is a tight loop and will not be good to have if there are a lot of messages from teh future
             std::thread::spawn(move || {
-                for env in boc {
+                for env in chan {
                     let c_iter = c_iter.load(Ordering::SeqCst);
                     if env.iteration < c_iter {
                         error!("Received message with iteration number that precedes current iteration number");
@@ -50,21 +47,40 @@ impl Orderer {
                     } else if env.iteration == c_iter {
                         aic.send(env.data).unwrap();
                     } else {
-                        bic.send(env).unwrap();
+                        //bic.send(env).unwrap();
+                        buffer
+                            .lock()
+                            .unwrap()
+                            .entry(env.iteration)
+                            .or_insert(vec![])
+                            .push(env.data)
                     }
                 }
             });
         }
 
-        (Orderer { cur_iteration }, arrival_out)
-    }
-
-    /// Increments the iteration
-    pub fn next_iteration(&mut self) {
-        self.cur_iteration.fetch_add(1, Ordering::SeqCst);
+        (
+            Orderer {
+                cur_iteration,
+                buffer,
+                sink: arrival_in,
+            },
+            arrival_out,
+        )
     }
 
     pub fn set_iteration(&mut self, i: usize) {
-        self.cur_iteration.store(i, Ordering::SeqCst)
+        self.cur_iteration.store(i, Ordering::SeqCst);
+
+        let mut buffer = self.buffer.lock().unwrap();
+        match buffer.get_mut(&self.cur_iteration.load(Ordering::SeqCst)) {
+            Some(msgs) => {
+                for msg in msgs.iter() {
+                    self.sink.send(msg.clone()).unwrap();
+                }
+                *msgs = vec![];
+            }
+            None => (),
+        }
     }
 }
